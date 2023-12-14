edd7a3e027c0d1e722af3c28e99c3f1c3de9eefb
ShikshaSingh9581 <153091897+ShikshaSingh9581@users.noreply.github.com>
2023-12-14 15:26:50 +0530

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,15 @@
+//write a code to print different types of datatype
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=10;
+    bool b=true;
+    float f=10.5;
+    string s="coding";
+    cout << x << endl;
+    cout << b << endl;
+    cout << f << endl;
+    cout << s << endl;
+    return 0;
+}
